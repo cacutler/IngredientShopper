f40a4cfba4837c51ac84cda3158f02cc293e8cb8
cacutler <116305038+cacutler@users.noreply.github.com>
2023-10-10 15:54:49 -0600

    Initial Commit

--- IngredientShopper/ViewController.swift
+++ IngredientShopper/ViewController.swift
@@ -1,19 +1,10 @@
-//
 //  ViewController.swift
 //  IngredientShopper
-//
 //  Created by Cameron Alexander Cutler on 10/10/23.
-//
-
 import UIKit
-
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
